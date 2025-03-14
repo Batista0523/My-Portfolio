0a0e36365138fb26815f3a755be50d25f83b5470
batista0523 <elisaulbatista@pursuit.org>
2025-03-13 22:05:27 -0400

    added

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', 
+    'react/display-name': 'off' ,
+  },
+  settings: {
+    react: {
+      version: 'detect',  
+    },
+  },
+};
